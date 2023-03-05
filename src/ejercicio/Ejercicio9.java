073a22b3a06b649fb14e8d94c90ab438af13d22f
sanch <sanch@DESKTOP-33ATQB5>
2023-03-05 20:18:35 +0100

    Ejercicio codificación

--- src/ejercicio/Ejercicio9.java
+++ src/ejercicio/Ejercicio9.java
@@ -4,33 +4,60 @@ import java.util.Scanner;
 
 public class Ejercicio9 {
 
-	char codifica(char conjunto1[],char conjunto2[], char c) {
-		char result=c;
-		String conjunt1 = String.valueOf(conjunto1);
-		String conjunt2 = String.valueOf(conjunto2);
+	/**
+	 * Método para intercambiar los caracteres
+	 * @param conjunto1
+	 * @param conjunto2
+	 * @param c
+	 * @return Devuelve el cacrater codificado
+	 */
+	static char codifica(char conjunto1[],char conjunto2[], char c) {
+		char result=c;//Se guardará el carácter
+		
+		//Recorremos el conjunto1
+		for (int i =0; i<conjunto1.length;i++) {
+			//Si el contenido de la posición i es igual a result
+			if (conjunto1[i]==result) {
+				//Se cambiará por el de la misma posición del conjunto2
+				result	=conjunto2[i];
+			}
+		}
+		//Devolvemos el resultado
 		return result;
 	}
 
 	public static void main(String[] args) {
-	char [] conjunto1= {'e','i','k','m','p','q','r','s','t','u','v'};
-	char [] conjunto2= {'p','v','i','u','m','t','e','r','k','q','s'};
-	String palabra;
-	String codif="";
-	Scanner read= new Scanner (System.in);
+		
+		//Declaramos y damos valores a los arrays
+		char [] conjunto1= {'e','i','k','m','p','q','r','s','t','u','v'};
+		char [] conjunto2= {'p','v','i','u','m','t','e','r','k','q','s'};
+		
+		//Palabra a codificar
+		String palabra;
+		
+		//Caracter codificado
+		char codificado;
 	
-	System.out.println("Introduzca una palabra para codificarla:");
-	palabra = read.next();
-	palabra.toLowerCase();
+		//Creamos Scanner 
+		Scanner read= new Scanner (System.in);
 	
-	for (int i =0; i<palabra.length();i++) {
-		for (int j = 0; j<conjunto1.length;j++) {
-			if (palabra.charAt(i)==conjunto1[j]) {
-				codif=palabra.replace(conjunto1[j], conjunto2[j]);
-			}
+		//Solicitamos la palabra
+		System.out.println("Introduzca una palabra para codificarla:");
+		//Leemos la palabra
+		palabra = read.next();
+		//Convertimos la palabra a minúsculas
+		palabra.toLowerCase();
+	
+		//Recorremos la palabra
+		for (int i =0; i<palabra.length();i++) {
+			//Se guardará el resultado de la función
+			codificado=Ejercicio9.codifica (conjunto1,conjunto2,palabra.charAt(i));
+			//Mostraremos caracter a caracter
+			System.out.print(codificado);
 		}
-	}
-	System.out.println(codif);
-	read.close();
+	
+		//Cerramos el Scanner 
+		read.close();
 	}
 
 }
